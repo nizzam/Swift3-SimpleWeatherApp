af88a240f3d9ab0be24ea10bc72b06cc482dfef3
montleo <apple-software@montleo.com>
2017-07-25 12:17:28 +0800

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Mont Leo on 25/07/2017.
+//  Copyright © 2017 Mont Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
